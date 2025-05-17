9d677851da35c46c10243151aaebce08a3171b46
namansingla-coder <namansingla@myyahoo.com>
2025-05-17 12:51:42 +0530

    t

--- nlp-service/start.sh
+++ nlp-service/start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-uvicorn app.main:app --host 0.0.0.0 --port 10001
+echo "Downloading model..."
+python app/sentiment.py  # This should download and extract your model
+echo "Starting FastAPI app..."
+uvicorn app.main:app --host 0.0.0.0 --port 10000
